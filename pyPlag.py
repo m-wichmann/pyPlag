e6c6c2c9b4c9600b4fae58ef5c933421f3ed2c74
Martin Wichmann <martinw@ichmann.de>
2012-06-15 17:21:54 +0200

    added license information

--- pyPlag.py
+++ pyPlag.py
@@ -1,6 +1,27 @@
 #!/usr/bin/python2.7
 # -*- coding: utf-8 -*-
 
+#####
+# pyPlag
+#
+# Copyright 2012, erebos42 (https://github.com/erebos42/miscScripts)
+#
+# This is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation; either version 2.1 of
+# the License, or (at your option) any later version.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this software; if not, write to the Free
+# Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+# 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+#####
+
 """check via google if file is a plagiarism"""
 
 # TODO:
